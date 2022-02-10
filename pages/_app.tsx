35da9b109ae8dafce5d3e0ae7f1f703b746f9b0c
malashevskyi <shevsky1488@gmail.com>
2022-02-10 15:32:21 +0200

    swr setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import '../styles/globals.css'
 import store from '../store'
 import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SWRConfig } from 'swr'
+
+const fetcher = (...args) => {
+  const [url] = args
+  return fetch(url as string).then((res) => res.json())
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <SWRConfig value={{ fetcher }}>
+          <Component {...pageProps} />
+        </SWRConfig>
       </ChakraProvider>
     </Provider>
   )
