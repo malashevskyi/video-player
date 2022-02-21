6da06f4b5bb492aa6f0a9c1e6d817dac13942040
malashevskyi <shevsky1488@gmail.com>
2022-02-21 16:25:40 +0200

    cancel video duration promises if component is unmounted

--- components/Modal/VideoProgressBar.tsx
+++ components/Modal/VideoProgressBar.tsx
@@ -9,56 +9,65 @@ const VideoProgressBar = () => {
   const dispatch = useDispatch()
   const [progressValue, setProgressValue] = useState<number>(0)
 
-  const findTotalDurations = useCallback(() => {
-    const uri = state.currentDirectory.slice(
-      state.currentDirectory.indexOf('file-manager')
-    )
-
-    const videoPromises = state.videoFilesOfCurrentDir.map((name) => {
-      return new Promise((resolve) => {
-        const video = document.createElement('video')
-        video.src = `/${uri}${name}`
-
-        video.onloadedmetadata = function () {
-          resolve(video.duration)
-        }
-      })
-    })
-    Promise.all(videoPromises).then((result) => {
-      if (result.includes(videoState.currentVideoDuration)) {
-        const totalVideosDuration = result.reduce(
-          (acc: number, item: number) => acc + item,
-          0
-        ) as number
-
-        dispatch(
-          videoModalActions.setTotalDurationOfFolderVideosInFolder(
-            totalVideosDuration
-          )
-        )
-        const currentVideoIndex = result.indexOf(
-          videoState.currentVideoDuration
-        )
-        const prevVideoDurations = result
-          .slice(0, currentVideoIndex)
-          .reduce((acc: number, item: number) => acc + item, 0) as number
-        dispatch(
-          videoModalActions.setTotalDurationOfPrevVideosInFolder(
-            prevVideoDurations
-          )
-        )
-
-        setProgressValue(
-          +(prevVideoDurations / totalVideosDuration).toFixed(2) * 100
-        )
-      }
-    })
-  }, [videoState.currentVideoDuration])
-
   useEffect(() => {
+    let isCancelled = false
     // write in state total duration of all prev videos after change a video
     if (videoState.currentVideoDuration) {
-      findTotalDurations()
+      const uri = state.currentDirectory.slice(
+        state.currentDirectory.indexOf('file-manager')
+      )
+
+      const videoPromises = state.videoFilesOfCurrentDir.map((name) => {
+        return new Promise((resolve, reject) => {
+          const video = document.createElement('video')
+          video.src = `/${uri}${name}`
+
+          video.onloadedmetadata = function () {
+            if (isCancelled) {
+              reject('component is unmounted')
+            }
+            resolve(video.duration)
+          }
+        })
+      })
+      Promise.all(videoPromises)
+        .then((result) => {
+          if (isCancelled) return
+          if (result.includes(videoState.currentVideoDuration)) {
+            const totalVideosDuration = result.reduce(
+              (acc: number, item: number) => acc + item,
+              0
+            ) as number
+
+            dispatch(
+              videoModalActions.setTotalDurationOfFolderVideosInFolder(
+                totalVideosDuration
+              )
+            )
+            const currentVideoIndex = result.indexOf(
+              videoState.currentVideoDuration
+            )
+            const prevVideoDurations = result
+              .slice(0, currentVideoIndex)
+              .reduce((acc: number, item: number) => acc + item, 0) as number
+            dispatch(
+              videoModalActions.setTotalDurationOfPrevVideosInFolder(
+                prevVideoDurations
+              )
+            )
+
+            setProgressValue(
+              +(prevVideoDurations / totalVideosDuration).toFixed(2) * 100
+            )
+          }
+        })
+        .catch((error) => {
+          console.log('error', error)
+        })
+    }
+
+    return () => {
+      isCancelled = true
     }
   }, [videoState.videoId, videoState.currentVideoDuration])
 
